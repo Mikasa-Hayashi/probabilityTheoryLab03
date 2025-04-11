a18abbee93388d871a5d70b56587531d22fd193d
Ivan <ivandenisovich2005@gmail.com>
2025-04-11 20:26:07 +0300

    Implemented main function

--- ProgramForTask01/main.py
+++ ProgramForTask01/main.py
@@ -1,4 +1,20 @@
-from task_solver import *
+from task_01_solver import *
+from task_02_solver import *
+
+
+def _select_task() -> None:
+    try:
+        task_number = int(input('Выберите номер задачи [1..2]: '))
+
+        if task_number == 1:
+            solve_task_01()
+        elif task_number == 2:
+            solve_task_02()
+        else:
+            raise ValueError
+    except ValueError:
+        print('Введен некорректный номер задачи')
+
 
 if __name__ == '__main__':
-    solve_task()
+    _select_task()
